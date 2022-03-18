6bcf1b11c21c593d7515587ab7e19837ff9fff20
Jesus Ovallos <jandresovallos@gmail.com>
2022-03-17 20:36:32 -0500

    Se adelanto el contenido del fichero salario.js

--- promedioPonderado.js
+++ promedioPonderado.js
@@ -1,3 +1,35 @@
-const notas = {
-    
-};
+const notas = [
+    {
+        course: "Cálculo",
+        note: 4.5,
+        credit: 4
+    },
+    {
+        course: "Programación",
+        note: 3.5,
+        credit: 5
+    },
+    {
+        course: "Física",
+        note: 4,
+        credit: 3
+    }
+];
+
+const noteWithCredit = notas.map(function (objeto){
+    return objeto.note * objeto.credit;
+});
+
+const sumNoteWithCredit = noteWithCredit.reduce(function (a = 0, b){
+    return a + b;
+});
+
+const credit = notas.map(function(objeto){
+    return objeto.credit;
+});
+
+const sumOfCredit = credit.reduce(function(sum = 0, b ){
+    return sum + b;
+});
+
+const promedioPonderado = parseFloat((sumNoteWithCredit / sumOfCredit).toFixed(2));
---
+++ salarios.js
@@ -0,0 +1,45 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000
+});
+colombia.push({
+    name: "Natalia",
+    salary: 1500
+});
+colombia.push({
+    name: "Carlos",
+    salary: 1000
+});
+colombia.push({
+    name: "Juanita",
+    salary: 8000
+});
+colombia.push({
+    name: "Pedro",
+    salary: 2000
+});
+colombia.push({
+    name: "Antonio",
+    salary: 2500
+});
+colombia.push({
+    name: "Laura",
+    salary: 1300
+});
+colombia.push({
+    name: "Alejandra",
+    salary: 3000
+});
+colombia.push({
+    name: "Rodrigo",
+    salary: 1900
+});
+colombia.push({
+    name: "Daniel",
+    salary: 3500
+});
+colombia.push({
+    name: "Esteban",
+    salary: 2150
+});
